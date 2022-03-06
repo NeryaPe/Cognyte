bd46d73f325af8afd8b35222652d664f4a36569f
NeryaPe <nerya.mamou@gmail.com>
2022-03-06 19:00:44 +0200

    add

--- cognyte_test.groovy
+++ cognyte_test.groovy
@@ -9,7 +9,7 @@ pipeline {
 
         text(name: 'IP', defaultValue: '', description: 'Enter some information about the VM name / IP')
        
-        file(name: "FILE", description: 'Choose path to upload file1.zip from local system.') 
+       // file(name: "FILE", description: 'Choose path to upload file1.zip from local system.') 
           //file(name: "FILE", file: "file1.zip", description: 'Choose path to upload file1.zip from local system.') 
 
        // text(name: 'FILE', defaultValue: '', description: 'file upload - the user should be able to upload ssh-public-key file for the new employee.')
@@ -21,7 +21,27 @@ pipeline {
 
         //password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
     }
+
+
+    
     stages {
+        //
+        stage('Get properties file from user and send to slave workspace') {
+                steps {
+                      
+                            script{
+                                    // Get file using input step, will put it in build directory
+                                    print "=================Please upload your property files ====================="
+                                    def inputFile = input message: 'Upload file', parameters: [file(name: 'global.properties')]
+                                    // Read contents and write to workspace
+                                    writeFile(file: 'global.properties', text: inputFile.readToString())
+                                    // Stash it for use in a different part of the pipeline
+                                    stash name: 'data', includes: 'global.properties'
+                            }
+                      
+                }
+            }
+            //
         stage('Example') {
             steps {
                 echo "Hello"
@@ -32,7 +52,7 @@ pipeline {
 
                 echo "VM name / IP: ${params.IP}"
 
-                mv my_file ${my_file}
+               // mv my_file ${my_file}
 
 
             }
